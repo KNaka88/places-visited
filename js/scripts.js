a3898fcfd469f5d218bc64f1c7ab2535f96e4a38
Erica Wright and Koji Nakagawa <koji@>
2017-01-23 09:55:21 -0800

    display details of places in a list

--- js/scripts.js
+++ js/scripts.js
@@ -15,9 +15,18 @@ $(function(){
     var landmark = $("input#landmark").val();
     var timeOfYear = $("input#timeofyear").val();
     var notes = $("textarea#notes").val();
+    var placesVisited = new PlaceVisited(placeName, landmark, timeOfYear, notes);
 
   $("div #show-result").show();
-    $("#show-result ul").append("<li><span class='places'>" + placeName + "</span></li>");
+    $("#show-result ul").append("<li><span class='placelist'>" + placesVisited.placeName + "</span></li>");
+
+    $(".placelist").last().click(function() {
+      $("#results-detail").show();
+      $(".name-of-place").text(placesVisited.placeName);
+      $(".place-landmark").text(placesVisited.landmark);
+      $(".time-place-visited").text(placesVisited.timeOfYear);
+      $(".place-notes").text(placesVisited.notes);
+    });
     event.preventDefault();
   });
 });
